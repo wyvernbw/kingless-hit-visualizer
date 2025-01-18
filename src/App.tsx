bec7741d6c5070fc7a49ff0ee659957de60b4530
wyvernbw <iamwyvern511@gmail.com>
2025-01-18 18:17:12 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from './components/theme-toggle';
+
+export const App = () => {
+	return (
+		<div className="h-screen w-screen p-4 font-serif text-[2rem]">
+			<main className="h-full w-full items-center place-content-center flex flex-col">
+				Hello world!
+			</main>
+		</div>
+	);
+};
